1a06c66751915b90cd7ed925d61ce546777005b3
Pratik Nayak <pratikvn@protonmail.com>
2023-05-13 15:43:57 +0200

    update scripts

--- frontier/postprocess.sh
+++ frontier/postprocess.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -l
 
 declare -a MECHS=("dodecane_lu_qss")
-declare -a NTASKS=("16")
-declare -a SOLVERS=("ginkgo_BICGSTAB")
+declare -a NTASKS=("1" "8" "64")
+declare -a SOLVERS=("ginkgo_BICGSTAB" "ginkgo_GMRES" "magma_direct")
 
 
 set -x
@@ -18,8 +18,10 @@ do
 	echo "psize,bsize,ntasks,min,avg,max" > $fname
 		for nt in "${NTASKS[@]}"
 		do
+			psize=$((64*64*64*$nt))
+			bsize=$((64*64*64)) 
 			out_time=$(awk '/^TinyProfiler/ {$1=""; print $0}' ReactEval.$s.64* | tr ' ' '\n' | perl -MScalar::Util -ne 'Scalar::Util::looks_like_number($_) && print' | tr '\n' ',')
-			echo ",64,16,,$out_time" >> $fname
+			echo "$psize,$bsize,$nt,$out_time" >> $fname
 		done
 	done
 	popd
--- frontier/prepare_and_submit.sh
+++ frontier/prepare_and_submit.sh
@@ -7,29 +7,37 @@ set -e
 
 #declare -a SOLVERS=("magma_direct" "ginkgo_BICGSTAB" "ginkgo_GMRES")
 #declare -a N_TASKS=("16" "32" "64" "128")
+#declare -a N_TASKS=("1" "8" "64" "512" "4096")
 #declare -a MAX_GRID_SIZE=("32" "64")
-declare -a SOLVERS=("ginkgo_GMRES")
-declare -a N_TASKS=("16")
-declare -a GRID_SIZE=("32")
-
-echo "#!/bin/bash" > $bf
-echo "#SBATCH -A CSC326" >> $bf
-echo "#SBATCH -J Ginkgo-Pele" >> $bf
-echo "#SBATCH -o %x-%j.out" >> $bf
-echo "#SBATCH -t 00:10:00" >> $bf
-echo "#SBATCH -p batch" >> $bf
-
+declare -a SOLVERS=("ginkgo_GMRES" "ginkgo_BICGSTAB" "magma_direct")
+declare -a N_TASKS=("1" "8" "64" "512" "4096")
+declare -a GRID_SIZE=("64")
+base_size="64"
 
 for s in "${SOLVERS[@]}"
 do
 	for nt in "${N_TASKS[@]}"
 	do
-		echo "#SBATCH -N $(($nt/8))" >> $bf
-		for bsize in "${GRID_SIZE[@]}"
+		num_nodes=$(($nt/8))
+		if [[ $num_nodes < 1 ]];
+		then
+			num_nodes=1
+		fi
+		for batch_size in "${GRID_SIZE[@]}"
 		do
-			c_string="128 128 128"
-			echo "SOLVER=$s NTASKS=$nt NCELLS=\"$c_string\" MAX_GRID_SIZE=$bsize ./run-reacteval.sh" >> $bf
+			echo "#!/bin/bash" > $bf
+			echo "#SBATCH -A CSC326" >> $bf
+			echo "#SBATCH -J Ginkgo-Pele-$s-$nt" >> $bf
+			echo "#SBATCH -o %x-%j.out" >> $bf
+			echo "#SBATCH -t 00:10:00" >> $bf
+			echo "#SBATCH -p batch" >> $bf
+			echo "#SBATCH -N $num_nodes" >> $bf
+			mult=$(echo $nt | awk '{ print $1^(1/3) }')
+			c_string=$(yes "$((${base_size} * $mult))" | head -3 | xargs echo)
+			echo "SOLVER=$s NTASKS=$nt NCELLS=\"$c_string\" MAX_GRID_SIZE=$batch_size ./run-reacteval.sh" >> $bf
+			sbatch $bf
 		done
 	done
 done
+
 
